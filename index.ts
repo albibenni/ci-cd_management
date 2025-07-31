166413d5b6da6484cd78139937f2c92e70da86c3
Alberto Benatti <albi.benni8@gmail.com>
2025-07-31 11:11:49 +0200

    wip cli for automating processes

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
+import Yargs from "yargs";
+
 type BuildJSON = {
   name: string;
   serviceType: string;
 };
+
+function main() {
+  const argv = Yargs(process.argv.slice(2)).argv;
+  console.log(argv);
+}
+
+main();
