10bdbb4ccaffbb25115983c26ed0cc6fc86abce0
Alberto Benatti <albi.benni8@gmail.com>
2025-08-01 15:10:20 +0200

    fix yargs with better handling

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import Yargs from "yargs";
+import yargs from "yargs";
 import { configSchema, OPTIONS } from "./types.ts";
 
 function main() {
-  const argv = Yargs(process.argv.slice(2)).argv;
+  const argv = yargs(process.argv.slice(2))
+    .option(OPTIONS.CONFIG, {
+      type: "string",
+      description: "Configuration file path",
+    })
+    .parseSync();
 
   const result = configSchema.safeParse(argv[OPTIONS.CONFIG]);
 
   if (!result.success) {
-    throw new Error("Not found");
+    throw new Error(result.error.message);
   }
   console.log(result.data);
 }
